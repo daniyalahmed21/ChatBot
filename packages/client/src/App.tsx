8f438fa8034c74c69b11d4ccbb37216fbdd34359
Daniyal Ahmed <da9033431@gmail.com>
2025-10-09 16:10:30 +0100

    Shadcn setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [message, setMessage] = useState('Hello Vite + React!')
+  const [message, setMessage] = useState("Hello Vite + React!");
   return (
     useEffect(() => {
       fetch("/api/hello")
         .then((response) => response.json())
         .then((data) => setMessage(data.message));
     }, []),
-    <div>
-      <h1 className='font-bold text-3xl'>{message}</h1>
-    </div>
-  )
+    (
+      <div>
+        <h1 className="font-bold text-3xl">{message}</h1>
+        <div className="flex flex-col justify-center items-center min-h-svh">
+          <Button>Click me</Button>
+        </div>
+      </div>
+    )
+  );
 }
 
-export default App
+export default App;

2ae2a5756746da894c3fafb97bcd62b928ebff4e
Daniyal Ahmed <da9033431@gmail.com>
2025-10-09 14:59:00 +0100

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from the API!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at Port http://localhost:${PORT}`);
 });

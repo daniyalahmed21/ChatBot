4357c3e2cc6760fba335b536a9c4d70221a4cbae
Daniyal Ahmed <da9033431@gmail.com>
2025-10-10 22:03:33 +0100

    Gemini setup

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,42 @@
-import dotenv from "dotenv";
-import express from "express";
-import type { Request, Response } from "express";
-
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
+import express from 'express';
+import type { Request, Response } from 'express';
+dotenv.config();
 const app = express();
-dotenv.config()
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
-});
+const client = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello from the API!" });
+app.post('/api/chat', async (req: Request, res: Response) => {
+    try {
+        const { prompt } = req.body || {};
+
+        if (!prompt) {
+            return res.status(400).json({ error: 'Prompt is required' });
+        }
+
+        const model = client.getGenerativeModel({
+            model: 'gemini-2.5-flash-lite',
+            generationConfig: {
+                temperature: 0.2,
+                maxOutputTokens: 100,
+            },
+        });
+        const result = await model.generateContent(prompt);
+
+        res.json({
+            message: result.response.text(),
+        });
+    } catch (error: any) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Failed to generate response' });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at Port http://localhost:${PORT}`);
+    console.log(`Server running at Port http://localhost:${PORT}`);
 });

4460403be0a1228fbd26a6ab2f84998dfa0745ee
Daniyal Ahmed <da9033431@gmail.com>
2025-10-11 18:18:07 +0100

    React hook from setup

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,20 +1,51 @@
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa6';
+import { useForm } from 'react-hook-form';
+import React from 'react';
+
+type formData = {
+    prompt: string;
+};
 
 const ChatBot = () => {
+    const { register, handleSubmit, reset, formState } = useForm<formData>();
+
+    const onSubmit = (data: formData) => {
+        console.log(data);
+        reset();
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleSubmit(onSubmit)();
+        }
+    };
+
     return (
-        <div className="flex flex-col gap-3 shadow-lg mt-auto p-4 border rounded-2xl w-full overflow-hidden">
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-3 shadow-lg mt-auto p-4 border rounded-2xl w-full overflow-hidden"
+        >
             <textarea
+                {...register('prompt', {
+                    required: true,
+                    validate: (value) => value.trim().length > 0,
+                })}
                 maxLength={1000}
                 placeholder="Type your message..."
                 className="px-3 py-2 border rounded-xl outline-none w-full h-24 text-zinc-800 placeholder:text-zinc-500 text-sm resize-none"
             />
             <div className="flex justify-end">
-                <Button className="bg-zinc-900/90 hover:opacity-80 rounded-full w-9 h-9 font-medium text-white transition-all duration-200">
+                <Button
+                    disabled={!formState.isValid}
+                    className="bg-zinc-900/90 hover:opacity-80 rounded-full w-9 h-9 font-medium text-white transition-all duration-200"
+                >
                     <FaArrowUp />
                 </Button>
             </div>
-        </div>
+        </form>
     );
 };
 
